23aee6f530aff0a0c7321d290d4a0a7d6e8ea10a
Nic Doro <nicola.doronzo95@gmail.com>
2019-04-08 15:18:24 +0300

    Style ready and animated menu

---
+++ dist/main.js
@@ -0,0 +1,56 @@
+//document queries
+const threeLinesMenu = document.querySelector('.menu-btn');
+const threeLines = document.querySelectorAll('.line');
+const socialMenu = document.querySelector('#social');
+const hiddenMenu = document.querySelector(".menu");
+const hiddenMenuNav = document.querySelector(".menu-nav");
+const hiddenMenuLink = document.querySelectorAll(".nav-link");
+
+let menuIsShown = false;
+
+threeLinesMenu.addEventListener("click", toggleMenu);
+
+
+function toggleMenu() {
+    if(!menuIsShown){
+
+        
+        threeLines.forEach(elem => {
+            
+            elem.classList.add("shown");
+        });
+        setTimeout( function() {
+            threeLines.forEach( elem => {
+            elem.classList.add("shown_second");
+            })
+        }, 500);
+
+        hiddenMenuNav.classList.add("shown");
+        hiddenMenuLink.forEach(elem => {
+            elem.classList.add("shown")
+        })
+        menuIsShown = true;
+        
+    } else {
+
+        setTimeout( function() {
+            threeLines.forEach( elem => {
+            elem.classList.remove("shown");
+            })
+        }, 500);
+
+        threeLines.forEach(elem => {
+            elem.classList.remove("shown_second");
+        })
+        
+        hiddenMenuNav.classList.remove("shown");
+        hiddenMenuLink.forEach(elem => {
+            elem.classList.remove("shown")
+        })
+        menuIsShown = false;
+        
+    }
+}
+
+
+
