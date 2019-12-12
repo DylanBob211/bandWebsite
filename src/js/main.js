adead4fa28b2aba32ac14ef74a33b25da8f86674
Nicola D'Oronzo <nicoladoronzo95@gmail.com>
2019-12-12 19:08:52 +0100

    contact-box add

--- src/js/main.js
+++ src/js/main.js
@@ -89,3 +89,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+
+document.querySelector('#contact').addEventListener('click', toggleContact);
+function toggleContact() {
+  const contactBox = document.querySelector('#contact-box');
+  if (!contactBox.classList.contains('shown')) {
+    contactBox.classList.add('shown');
+  } else {
+    contactBox.classList.remove('shown');
+  }
+
+}
